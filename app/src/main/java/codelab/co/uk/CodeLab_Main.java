dbe7afe556d1dd4a572aa94b043ce2f57f262044
PixeLab <DroidPreview@gmail.com>
2018-06-13 01:57:46 +0900

    First check is done ;
    : Main menu 100%
    : 3 GUI is preparing 0%
    : added logo.img for background screen [1]

---
+++ app/src/main/java/codelab/co/uk/CodeLab_Main.java
@@ -0,0 +1,13 @@
+package codelab.co.uk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CodeLab_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_lab__main);
+    }
+}
